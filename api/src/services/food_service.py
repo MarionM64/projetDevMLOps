aa338221153a3530d5053ee8b51631f94c89bc0d
MarionM64 <marion.marty@cy-tech.fr>
2026-01-26 10:50:15 +0100

    ajout de la recherhe de recette via api spoonacular

---
+++ api/src/services/food_service.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+# Charger les variables d'environnement depuis le fichier .env
+load_dotenv()
+
+# Récupérer la clé API
+API_KEY = os.getenv("API_FOOD_KEY")
+BASE_URL = "https://api.spoonacular.com/recipes"
+
+def rechercher_recettes(query):
+    endpoint = f"{BASE_URL}/complexSearch"
+    params = {
+        "apiKey": API_KEY,
+        "query": query,
+        'number': 10
+    }
+    response = requests.get(endpoint, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Erreur {response.status_code}: {response.text}")
+        return None
+
+
+def get_recette_par_id(id_recette):
+    endpoint = f"{BASE_URL}/{id_recette}/information"
+    params = {
+        "apiKey": API_KEY,
+        "includeNutrition": True
+    }
+    response = requests.get(endpoint, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Erreur {response.status_code}: {response.text}")
+        return None
+    
