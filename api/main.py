d93205ebc78fa280a72abb605393f7ec79e347d4
Waoutch <ma3leve@gmail.com>
2026-01-26 16:27:26 +0100

    add : init js in index.html

--- api/main.py
+++ api/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 import src.router.food_router as food_router
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(food_router.router)
 
